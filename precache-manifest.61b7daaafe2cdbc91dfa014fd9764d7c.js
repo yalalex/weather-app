2440527414b98e44c7b28dfecc8497aae751a2e6
AlexY <alexyo87@yahoo.com>
2019-08-08 19:44:23 +0300

    Updates

---
+++ precache-manifest.61b7daaafe2cdbc91dfa014fd9764d7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fb23dc791947ad6698d03b81e787dcd",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "73abf203a18b065064e4",
+    "url": "/weather-app/static/css/main.f65227a3.chunk.css"
+  },
+  {
+    "revision": "e73f2af2fb261b3d6a9a",
+    "url": "/weather-app/static/js/2.4c3971f8.chunk.js"
+  },
+  {
+    "revision": "73abf203a18b065064e4",
+    "url": "/weather-app/static/js/main.6a61c1eb.chunk.js"
+  },
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  }
+]);
