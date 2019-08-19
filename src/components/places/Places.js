ba88e0b8e4a8229e4a135918693e9c69cf43ba11
AlexY <alexyo87@yahoo.com>
2019-08-19 21:12:54 +0300

    grid fix

--- src/components/places/Places.js
+++ src/components/places/Places.js
@@ -8,7 +8,7 @@ const Places = ({ places, getForecast, loading, lang }) => {
     return <Spinner />;
   } else
     return (
-      <div className='grid-3'>
+      <div className='grid-4'>
         {places.map(place => (
           <PlaceItem
             key={place.id}
