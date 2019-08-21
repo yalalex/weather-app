d0aee8fac1a593f0e2a7b2bb8b1208946f26dbeb
AlexY <alexyo87@yahoo.com>
2019-08-21 19:33:38 +0300

    minor changes

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -21,7 +21,7 @@ const Navbar = ({ units, lang, switchLang, switchUnits }) => {
           <div className='switch'>
             <div className='select'>
               {units === 'metric' ? '°C' : '°F'}
-              <i className='fa fa-angle-down' aria-hidden='true' />
+              <i className='fa fa-angle-down' />
             </div>
             <ul className='select-drop'>
               <li>
@@ -36,7 +36,7 @@ const Navbar = ({ units, lang, switchLang, switchUnits }) => {
           <div className='switch'>
             <div className='select'>
               {lang === 'en' ? 'EN' : 'RU'}
-              <i className='fa fa-angle-down' aria-hidden='true' />
+              <i className='fa fa-angle-down' />
             </div>
             <ul className='select-drop'>
               <li>
